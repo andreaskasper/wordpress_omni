4e13713077907644e75df53f70ef26c8fec39035
Andreas Kasper <Andreas.Kasper@goo1.de>
2023-07-07 15:37:47 +0000

    Nice text for Byte Sizes in nagios/disk.json

--- src/goo1-omni/classes/NagiosDisk.php
+++ src/goo1-omni/classes/NagiosDisk.php
@@ -25,9 +25,24 @@ class NagiosDisk {
             $state = 1;
         }
 
-        Nagios::send($state, $status . "Free: " . $df . "/" . $ds. "; " . number_format($proz,1) . "% full");
+        header("Cache-Control: public, max-age=60, s-maxage=60");
+        Nagios::send($state, $status . "Free: " . self::format_bytes($df,1) . "/" . self::format_bytes($ds,1). "; " . number_format($proz,1) . "% full");
         exit;
     }
+
+    private static function format_bytes($bytes, $precision = 2) { 
+        $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+    
+        $bytes = max($bytes, 0); 
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+        $pow = min($pow, count($units) - 1); 
+    
+        // Uncomment one of the following alternatives
+        // $bytes /= pow(1024, $pow);
+        $bytes /= (1 << (10 * $pow)); 
+    
+        return round($bytes, $precision) . ' ' . $units[$pow]; 
+    }  
 
 
 }
