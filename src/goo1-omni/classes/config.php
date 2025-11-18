64861eb2dc8f378d2b38be00f3c9281a782bca8f
Andreas Kasper <Andreas.Kasper@goo1.de>
2025-11-18 07:48:25 +0000

    add documentation to the config.php

--- src/goo1-omni/classes/config.php
+++ src/goo1-omni/classes/config.php
@@ -2,15 +2,34 @@
 
 namespace plugins\goo1\omni;
 
+/**
+ * Configuration management class for Goo1 Omni plugin
+ *
+ * @package plugins\goo1\omni
+ * @author goo1
+ */
 class config {
 
     private static $_data = null;
 
+    /**
+     * Retrieves a configuration value by name.
+     *
+     * @param string $name The name of the configuration parameter to retrieve
+     * @return mixed The configuration value, or null if not found
+     */
     public static function get($name) {
         self::load0();
         return self::$_data[$name] ?? null;
     }
 
+    /**
+     * Sets a configuration value by name.
+     *
+     * @param string $name The configuration key/name to set
+     * @param mixed $value The value to assign to the configuration key
+     * @return bool True on success
+     */
     public static function set($name, $value) {
         self::load0();
         self::$_data[$name] = $value;
@@ -19,6 +38,15 @@ class config {
     }
 
 
+    /**
+     * Loads the initial configuration settings.
+     * 
+     * This private static method is responsible for loading the base configuration
+     * during the initialization process. It serves as the first step in the
+     * configuration loading sequence.
+     *
+     * @return void
+     */
     private static function load0() {
         if (!is_null(self::$_data)) return self::$_data;
         self::$_data = get_site_option( "_goo1_omni_configs", null , true);
